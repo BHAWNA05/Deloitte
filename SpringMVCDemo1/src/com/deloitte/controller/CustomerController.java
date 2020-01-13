fc8e4cbeb19d2ecedbad50467ef2a4105220195b
BHAWNA05 <38226651+BHAWNA05@users.noreply.github.com>
2020-01-13 16:55:50 +0530

    Add files via upload

---
+++ SpringMVCDemo1/src/com/deloitte/controller/CustomerController.java
@@ -0,0 +1,49 @@
+package com.deloitte.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cms.deloitte.dao.CustomerDAO;
+import com.cms.deloitte.dao.impl.CustomerDAOImpl;
+import com.cms.deloitte.model.Customer;
+
+@Controller
+public class CustomerController {
+	
+	@Autowired
+	CustomerDAO CustomerDAO;
+	@RequestMapping("customerSave")
+	public ModelAndView saveCustomerDetails(Customer customer) {
+		System.out.println(customer);
+		CustomerDAO.addCustomer(customer);
+		CustomerDAO.updateCustomer(customer);
+		ModelAndView view = new ModelAndView();
+		view.setViewName("result");
+		view.addObject("custo", customer);
+		return view;
+	}
+		
+		@RequestMapping("updateCustomer") 
+		public ModelAndView updateCustomerDetails(Customer customer)
+	    {
+			
+			ModelAndView view = new ModelAndView();
+			view.addObject("custo", customer);
+		if(CustomerDAO.isCustomerExists(customer.getCustomerId()))
+		{
+			CustomerDAO.updateCustomer(customer);
+		view.setViewName("result");
+		
+		}
+		else
+		{
+		view.setViewName("error");	
+		}
+		
+		return view;
+	}
+	
+}
+ 
